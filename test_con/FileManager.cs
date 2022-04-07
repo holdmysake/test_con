0b68b8f4b5ca0a607c092776963534756bb1dafa
Mateusz Gąsiorek <fikserfredpl@gmail.com>
2022-04-07 09:05:02 +0200

    Dodaj pliki projektów.

---
+++ test_con/FileManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_con
+{
+    public class FileManager
+    {
+        private StringBuilder path = new StringBuilder();//path for directory 
+        public FileManager()//constructor
+        {
+
+        }
+        public void directory()//directory creator method
+        {
+            Console.Write("Please, indicate path: ");
+            path.Append(Console.ReadLine());
+            Console.WriteLine($"Creating folder. . .{path}");
+            DirectoryInfo DI = Directory.CreateDirectory(path.ToString());
+        }
+        public void writeTo()//Writing to file method
+        {
+            if (!File.Exists(path.ToString()))
+            {
+                using (StreamWriter sw = new StreamWriter(path.ToString()))
+                {
+                    sw.WriteLine("Siemaneczko\nZiomeczki");
+                }
+            }
+        }//
+        public void readFrom()//reading from file method
+        {
+            using (StreamReader sr = new StreamReader(path.ToString()))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ test_con/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_con
+{
+    public class Program
+    {
+        static public void main()
+        {
+            FileManager obj1 = new FileManager();
+            obj1.directory();
+        }
+    }
+}
