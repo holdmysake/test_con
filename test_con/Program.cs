bf85f6b73322025bc0ff6dad4a379cb005d59a23
Mateusz Gąsiorek <fikserfredpl@gmail.com>
2022-04-26 17:42:42 +0200

    Added test for class1

--- test_con/Program.cs
+++ test_con/Program.cs
@@ -11,11 +11,20 @@ namespace test_con
     {
         static public void Main()
         {
+            Lista obj = new Lista();
+            obj.Add("Staura");
+            obj.Add("Max");
+            obj.Add("Maxxxx");
+            obj.Add("Staura");
+            obj.Add("XD");
+            obj.Add("Staura");
+            obj.DisplayAll();
 
+            /*
             string userIn = " ";
             userIn = Console.ReadLine();  
             Challenge1 chall = new Challenge1(userIn);
-            /*
+            
             FileManager obj1 = new FileManager();
             Something som1 = new Something();
             som1.useall();//obj1.directory();
